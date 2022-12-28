5214f5cc0f59ca6442976d1f975f6490c194e425
Srijana Poudel <srijana.poudel@tuni.fi>
2022-12-28 23:30:57 +0200

    connected mongodb database

--- backend/src/api/todos.js
+++ backend/src/api/todos.js
@@ -1,89 +1,20 @@
 const express = require('express');
+const { getTodos, getTodo, createTodo, updateTodo, deleteTodo } = require('../controllers/todoControllers');
 const router = express.Router();
 
-let todos = [
-    {
-      id: 1,
-      text: 'Study',
-      done: true
-    },
-    {
-      id: 2,
-      text: 'Work',
-      done: true
-    },
-    {
-      id: 3,
-      text: 'Play',
-      done: true
-    },
-];
 
-router.get('/', (req, res) => {
-  res.status(200).json(todos);
-});
+router.get('/', getTodos);
 
 
-router.get('/:id', (req, res) => {
-    const { id } = req.params;
-    console.log(id);
-    const todo = todos.find((m) => m.id === Number(id));
-    
-    if (todo) {
-        res.status(200).json(todo);
-    } else {
-        res.status(404).json({ message: 'Not found' });
-    }
-  });
+router.get('/:id', getTodo);
 
-router.post('/', (req, res) => {
-    const { text } = req.body;
-    let maxId = 0;
-
-    if (todos.length > 1) {
-      maxId = todos.reduce(function(prev, current) {
-        return (prev.id > current.id) ? prev.id : current.id
-      })
-    } else if (todos.length === 1) {
-      maxId = todos[0].id;
-    }
-
-    const newTodo = { id: maxId + 1, text, done: false };
-    todos.push(newTodo);
-    res.status(201).json(newTodo);
-  });
+router.post('/', createTodo);
 
 
-router.patch('/:id', (req, res) => {
-    const { id } = req.params;
-    const { text, done } = req.body;
-    const index = todos.findIndex((m) => m.id === Number(id));
-    const updatedTodo = {
-      id: Number(id),
-      text,
-      done
-    };
-    todos[index] = updatedTodo;
-    res.status(200).json({ message: 'Updated' });
-  });
+router.patch('/:id', updateTodo);
 
 
-router.delete('/:id', (req, res) => {
-  const { id } = req.params;
-  
-  const deletedTodo = todos.filter(todo => {
-    return todo.id === Number(id)
-  });
-
-  const updatedTodos = todos.filter((b) => {
-      return b.id !== Number(id)
-    });
-
-    todos = updatedTodos;
-
-  console.log('deletedTodo ', deletedTodo);
-  res.status(200).json(deletedTodo[0]);
-});
+router.delete('/:id', deleteTodo);
   
 
 module.exports = router;
--- backend/src/app.js
+++ backend/src/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const morgan = require('morgan');
 const helmet = require('helmet');
 const cors = require('cors');
+const mongoose = require('mongoose')
 
 require('dotenv').config();
 
@@ -14,6 +15,22 @@ app.use(morgan('dev'));
 app.use(helmet());
 app.use(cors());
 app.use(express.json());
+
+// Connect to db
+
+mongoose.connect('mongodb+srv://Srijana:poudel1@cluster1.dhvt5yi.mongodb.net/?retryWrites=true&w=majority')
+    .then(() => {
+
+        //listen for request
+        app.listen(process.env.PORT,() => {
+            console.log('Connected to db and listening on port', process.env.PORT)
+        
+        })
+    })
+    .catch((error) =>{
+        console.log(error)
+    })
+
 
 app.get('/', (req, res) => {
   res.json({
